c95880dfb34f23c4ba5ddf447647a4995ec3029e
Umar Gora <umarg1997@gmail.com>
2021-12-12 10:06:40 +0000

    simple error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	// Get a greeting message and print it.
-	message := greetings.Hello("Umar")
+	message, err := greetings.Hello("Umar")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
