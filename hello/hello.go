75aa58db95374d191390964898dfc3e80b024c40
Umar Gora <umarg1997@gmail.com>
2021-12-12 11:45:23 +0000

    working with slices and maps

--- hello/hello.go
+++ hello/hello.go
@@ -10,10 +10,16 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	// Get a greeting message and print it.
-	message, err := greetings.Hello("Umar")
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
